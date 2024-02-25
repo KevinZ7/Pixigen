ae8aeab09bf3b8da548bcf9b53d17bf733f95f2e
unknown <kevin.z.zhou@gmail.com>
2024-02-25 03:55:24 -0800

    fix URL type

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
-  photo: { type: URL, required: true },
+  photo: { type: String, required: true },
   firstName: { type: String },
   lastName: { type: String },
   planId: { type: Number, default: 1 },
